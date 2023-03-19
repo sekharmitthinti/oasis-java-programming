70094b2b385cd14e4811c5c94b590611213a8fd2
sekharmitthinti <123652430+sekharmitthinti@users.noreply.github.com>
2023-03-19 14:43:30 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = 1 + (int) (100 * Math.random());
+        System.out.println("Number of trails: ");
+        int K = sc.nextInt();
+        int i, guess;
+        System.out.println("A number is chosen between 1 to 100,Guess the number within " + K + " trails");
+        for (i = 0; i < K; i++) {
+            System.out.println("Guess the number:");
+            guess = sc.nextInt();
+            if (number == guess) {
+                System.out.println("Congratulations!.....You won the game.");
+                break;
+            } else if (number > guess && i != K - 1) {
+                System.out.println("The number is greater than " + guess);
+            } else if (number < guess && i != K - 1) {
+                System.out.println("The number is less than " + guess);
+            }
+        }
+
+        if (i == K) {
+            System.out.println("You have exhausted" + K + " trials.");
+            System.out.println("The number was " + number);
+        }
+    }
+}
